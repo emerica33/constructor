7adc0931b3450a1e4b8ca055fa159bcf40db9132
emerica33 <42452311+emerica33@users.noreply.github.com>
2018-10-03 15:44:27 -0300

    Metodo constructor

---
+++ src/Metodocons.java
@@ -0,0 +1,19 @@
+
+public class Metodocons {
+    public Metodocons (){
+        System.out.println("Metodo sin parametros.");
+    }
+    public Metodocons (int a){
+        System.out.println("Metodo con 1 parametros:"+a);
+      
+    }
+    public Metodocons (int a, int b){
+        System.out.println("Metodo con 2 parametros:"+a+", "+b);
+        
+    }
+    public Metodocons (int a, int b, String nombre){
+        System.out.println("Metodo con 3 parametros:"+a+", "+b+", "+nombre);
+
+    }
+    
+}
---
+++ src/principal.java
@@ -0,0 +1,13 @@
+
+public class principal {
+    
+    public static void main(String[] args) {
+        Metodocons metodo1 = new Metodocons();
+        Metodocons metodo2 = new Metodocons(1);
+        Metodocons metodo3 = new Metodocons(1, 2);
+        Metodocons metodo4 = new Metodocons(1, 2, "Gabriel");
+        
+        
+    }
+    
+}
